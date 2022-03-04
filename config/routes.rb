2bf0da069094b59232417bf6f2bea820126ca596
ravinder <ravinder@devohack.com>
2022-03-04 12:56:22 +0530

    Add subdomain to user and projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
+class SubdomainConstraint   
+  
+  def self.matches?(request)     
+    request.subdomain.present? && request.subdomain != 'www'   
+  end 
+end 
+
 Rails.application.routes.draw do
+  constraints SubdomainConstraint do     
+    resources :users   
+  end
+  resources :projects
+  # resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
